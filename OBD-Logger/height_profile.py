8c103bb8728bf528109ee1fd931c42ae90b50120
officalopossum <73652417+officalopossum@users.noreply.github.com>
2020-12-21 14:00:46 +0100

    Add files via upload
    Automatic index finding for used values
    Exception handling more commenting

--- OBD-Logger/height_profile.py
+++ OBD-Logger/height_profile.py
@@ -5,25 +5,52 @@ Version: 1.0
 Description:
     take .csv file
     extract necessary information and reformat (GPS_long, GPS_lat, GPS_alt, GPS_time)
-    calculate gradient, angle(rad), angle(degree)
-    save everything in np array
+    calculate normalized gradient
+    maximum gradient is set to counteract gps inaccuracy leading to high values in stationary state,
+    calculate angle(rad), angle(degree)
+
+    save everything in json file
+    return dict and json file path
+
 """
 
 import numpy as np
 import csv
+import json
 
 
-def build_height_profile(path="/home/pi/Studienarbeit_OBD_Datenlogger/OBD-Logger/"):
+def build_height_profile(path="/home/pi/Studienarbeit_OBD_Datenlogger/OBD-Logger/", verbose=0):
     """
     :param path: direct path to .csv file
     :type path: str
-    :return: array with all information
-    :rtype: np.ndarray()
+    :return: dictionary with all information, filename of json file
+    :rtype: dict, str
 
     take .csv file
     extract necessary information and reformat (GPS_long, GPS_lat, GPS_alt, GPS_time)
-    calculate gradient, angle(rad), angle(degree)
-    save everything in np array
+    Format:
+    0: TIME
+    1: SPEED
+    2: RPM
+    3:ENGINE_LOAD
+    4: MAF
+    5: AMBIANT_AIR_TEMP
+    6: RELATIVE_ACCEL_POS
+    7: COMMANDED_EQUIV_RATIO
+    8: FUEL_LEVEL
+    9: GPS_Long
+    10: GPS_Lat
+    11: GPS_Alt
+    12: GPS_Time
+    13: INTERNAL_AIR_TEMP
+    14: VIN
+
+    calculate normalized gradient
+    maximum gradient is set to counteract gps inaccuracy leading to high values in stationary state,
+    calculate angle(rad), angle(degree)
+
+    save everything in json file
+    return dict and json file path
     """
 
     # empty list about to be filled
@@ -41,34 +68,58 @@ def build_height_profile(path="/home/pi/Studienarbeit_OBD_Datenlogger/OBD-Logger
 
         for row in reader:
             if first_row:
-                print(row)
+                # check format of .csv file
+                # default values
+                gps_long_index = 9
+                gps_lat_index = 10
+                gps_alt_index = 11
+                gps_time_index = 12
+
+                # find index for necessary values in csv file
+                try:
+                    gps_long_index = find_index(row, "GPS_Long")
+                    print_debug(("gps_long_index :", gps_long_index), verbose=verbose)
+                    gps_lat_index = find_index(row, "GPS_Lat")
+                    print_debug(("gps_lat_index :", gps_lat_index), verbose=verbose)
+                    gps_alt_index = find_index(row, "GPS_Alt")
+                    print_debug(("gps_alt_index: ", gps_alt_index), verbose=verbose)
+                    gps_time_index = find_index(row, "GPS_Time")
+                    print_debug(("gps time index: ", gps_time_index), verbose=verbose)
+                except FileFormatInvalid as e:
+                    print(e.message)
+                    return None, None
+
+                # debug
+                print_debug(row, verbose)
+
                 first_row = False
                 continue
 
             # converting format and inserting in lists
-            # 0 handling can be added here
-            print(row)
+            # unreadable data will be temporarily set to 0.0
+            print_debug(row, verbose=verbose)
             try:
-                gps_alt_list.append(float(row[6]))
+                gps_alt_list.append(float(row[gps_alt_index]))
             except ValueError:
                 gps_alt_list.append(0.0)
             try:
-                gps_lat_list.append(float(row[5]))
+                gps_lat_list.append(float(row[gps_lat_index]))
             except ValueError:
                 gps_lat_list.append(0.0)
             try:
-                gps_long_list.append(float(row[4]))
+                gps_long_list.append(float(row[gps_long_index]))
             except ValueError:
                 gps_long_list.append(0.0)
             try:
-                gps_time_list.append(float(row[7]))
+                gps_time_list.append(float(row[gps_time_index]))
             except ValueError:
                 gps_time_list.append(0.0)
 
     # debug
-    # print(gps_alt_list)
+    print_debug(gps_alt_list, verbose=verbose)
 
     # cleaning up data
+    # linear interpolation of 0 values
     zero_handling(gps_alt_list)
     zero_handling(gps_lat_list)
     zero_handling(gps_long_list)
@@ -111,29 +162,33 @@ def build_height_profile(path="/home/pi/Studienarbeit_OBD_Datenlogger/OBD-Logger
         angle_rad_array[i] = angle_from_gradient(normalized_gradient_array[i], in_rad=True)
 
     # debug
-    print("gradient list", gradient_array)
-    print("normalized_gradient_list", normalized_gradient_array)
-    print("angle list rad", angle_rad_array)
-    print("angle list deg", angle_deg_array)
+    print_debug(("gradient list", gradient_array), verbose=verbose)
+    print_debug(("normalized_gradient_list", normalized_gradient_array), verbose=verbose)
+    print_debug(("angle list rad", angle_rad_array), verbose=verbose)
+    print_debug(("angle list deg", angle_deg_array), verbose=verbose)
 
     # build array with all information (more than necessary)
-    height_profile_array = np.array(
-        [gps_time_list,
-         gps_long_list,
-         gps_lat_list,
-         gps_alt_list,
-         angle_rad_array,
-         angle_deg_array,
-         normalized_gradient_array]
-    )
+    height_profile_dict = {
+        "time": gps_time_list,
+        "gps_long": gps_long_list,
+        "gps_lat": gps_lat_list,
+        "gps_alt": gps_alt_list,
+        "angle_rad": angle_rad_array.tolist(),
+        "angle_deg": angle_deg_array.tolist(),
+        "gradient": normalized_gradient_array.tolist()
+    }
+
+    # write information to json file
+    json_filename = path[:-4]+"_height_profile.json"
+    json.dump(height_profile_dict, open(json_filename, "w"), indent=4)
 
     # debug
-    # print(height_profile_array)
+    print_debug(height_profile_dict, verbose=verbose)
 
-    return height_profile_array
+    return height_profile_dict, json_filename
 
 
-def zero_handling(iterable):
+def zero_handling(iterable, verbose=0):
     # if first element = 0
     if iterable[0] == 0.0:
         count_up = 0
@@ -152,7 +207,7 @@ def zero_handling(iterable):
                 try:
                     if iterable[i + 1 + count_up] != 0.0:
                         iterable[i + 1] = (iterable[i+1+count_up] * count_up + iterable[i]) / (count_up + 1)
-                        print("zero handled")
+                        print_debug("zero handled", verbose=verbose)
                         break
                 except IndexError:
                     iterable[i+1] = iterable[i]
@@ -174,56 +229,6 @@ def normalize(value, maximum=0.15):
     return maximum * np.tanh(value/maximum)
 
 
-def normalize_iterable(iterable, maximum=0.15, keep_original=False, differential_mode=False):
-    """
-    description:
-    takes a iterable object, list or array
-    returns a normalized array of equal size
-    differences or absolute values are dampened:
-    1) near 0: f(x) = y
-    2) infinity: f(x) = maximum
-
-    usage:
-    1) overwrite your array
-        normalize_iterable(iterable=array, maximum=maximum)
-    2) keep original array
-        normalized_array = normalize_iterable(iterable=array, maximum=maximum, keep_orinal=True)
-
-    :param iterable: list or array
-    :type iterable: list or np.ndarray()
-    :param maximum: cutoff value
-    :type maximum: float
-    :param keep_original: overwrite or new array
-    :type keep_original: bool
-    :param differential_mode: either differences are filtered or absolute values
-    :type differential_mode: bool
-    :return: filtered array
-    :rtype: list or np.ndarray()
-    """
-    from copy import deepcopy as deepcopy
-    # only works with iterables with at least 2 elements
-    try:
-        iterable[1] = iterable[1]
-    except IndexError:
-        return iterable
-    if keep_original:
-        # new array
-        temp_iterable = deepcopy(iterable)
-    else:
-        temp_iterable = iterable
-
-    if differential_mode:
-        # second till last are changed, difference is low pass filtered
-        for i in range(len(temp_iterable)-1):
-            temp_iterable[i+1] = temp_iterable[i] + maximum * np.tanh((temp_iterable[i+1]-temp_iterable[i])/maximum)
-    else:
-        # every value is filtered
-        for i in range(len(temp_iterable)):
-            temp_iterable[i] = maximum * np.tanh((temp_iterable[i])/maximum)
-
-    return temp_iterable
-
-
 def angle_from_gradient(gradient, in_rad=True):
     """
     calculates angle from gradient
@@ -242,10 +247,41 @@ def angle_from_gradient(gradient, in_rad=True):
         return np.arctan(gradient)*180 / np.pi
 
 
+def find_index(row, string):
+    """
+    returns the index of the matching element in a list
+    """
+    i = 0
+    for element in row:
+        if element == string:
+            return i
+        else:
+            i = i+1
+    raise FileFormatInvalid(path, "{} was not found in file {}. check csv file!".format(string, path))
+
+
+class FileFormatInvalid(Exception):
+    """
+    Exception to express an invalid FileFormat
+    """
+    def __init__(self, filename, message):
+        Exception.__init__(self)
+        self.filename = filename
+        self.message = message
+
+
+def print_debug(to_print, verbose=1):
+    """
+    enable, disable prints based on verbose value
+    """
+    if verbose > 0:
+        print(to_print)
+
+
 if __name__ == '__main__':
     # windows debugging path
-    path = r"C:\Users\Max\Documents\_5.Semester\Temp\test.csv"
+    path = r"C:\Users\Max\Documents\_5.Semester\Studienarbeit\test.csv"
 
     # pi path
     # path = "/home/pi/Studienarbeit_OBD_Datenlogger/OBD-Logger/"
-    height_profile = build_height_profile(path)
+    height_profile = build_height_profile(path, verbose=0)
