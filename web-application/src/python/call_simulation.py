b10e8f070b5060cc9391f537842af07df993ec93
Tim-Hager <64630117+Tim-Hager@users.noreply.github.com>
2021-03-20 16:37:52 +0100

    Python file to handle simulation script

---
+++ web-application/src/python/call_simulation.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 13 20:56:35 2021
+
+author: Tim Hager
+
+description: Python script to execute the simulation script.
+"""
+import sys
+argument = sys.argv[1]
+print(argument)
