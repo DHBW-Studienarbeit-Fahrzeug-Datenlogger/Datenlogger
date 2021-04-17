b4a9126fa4cc223fabac701f2f9bf30776b15afb
Tim-Hager <64630117+Tim-Hager@users.noreply.github.com>
2021-04-17 16:08:18 +0200

    Update driving_simulation.py
    The current date is inserted into the database

--- MySQL-server/driving_simulation.py
+++ MySQL-server/driving_simulation.py
@@ -267,7 +267,7 @@ def virtual_drive(car_id, route_id):
         "INSERT INTO  simulation ( filename, date, starttime, totalKM, endtime, VIN, fuelConsumption, " \
         + "energyConsumption, endLat, endLong, endDate, additional_file, id_car, id_route, filename_energy) VALUES ('"
         + str(route_information[1]) \
-        + "', '" + str(route_information[2]) + "', '" + str(route_information[3]) \
+        + "', '" + str(datetime.datetime.now()).split(" ")[0] + "', '" + str(route_information[3]) \
         + "', '" + str(route_information[4]) + "', '" + str(route_information[5]) \
         + "', '" + str(route_information[6]) + "', '" + str(route_information[7]) \
         + "', '" + str(route_information[8]) + "', '" + str(route_information[9]) \
